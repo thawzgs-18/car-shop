4f3e6252552591c8fb66c0f61c9b0fadaf1bfc74
Thawzgs <thawzgs18@gmail.com>
2026-04-04 14:57:52 +0700

    Remove redundant post button and cleanup header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.gianhangvn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a.storyblok.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kenhxehyundai.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd1hv7ee95zft1i.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'vcdn1-vnexpress.vnecdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img1.oto.com.vn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.usnews.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
